1adc96bc7cb5813c75c7cb768caa28802cc9bea5
Alireza <alirezaaa1194@gmail.com>
2025-03-11 00:34:03 +0330

    add supabase files

--- server.js
+++ server.js
@@ -1,6 +1,41 @@
+// const dotenv = require("dotenv");
+// const { default: mongoose } = require("mongoose");
+// const app = require("./app");
+
+// //* Load env
+// dotenv.config();
+
+// //* Database connection
+// (async () => {
+//   try {
+//     // const conn = await mongoose.connect(process.env.MONGO_URI,{
+//     //   authSource:'admin',
+//     //   useNewUrlParser:true,
+//     //   useUnifiedTopology:true
+//     // });
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (err) {
+//     //?error catch
+//     console.log(err);
+//     process.exit(1);
+//   }
+// })();
+
+// // const port = +process.env.PORT || 3000;
+// const port = 4000;
+
+// const productionMode = process.env.NODE_ENV === "production";
+// app.listen(port, () => {
+//   console.log(`Server running in ${productionMode ? "production" : "development"} mode on port ${port}`);
+// });
+
+////////////////////////////////////////////////////
+
+
 const dotenv = require("dotenv");
-const { default: mongoose } = require("mongoose");
-const app = require("./app");
+const mongoose = require("mongoose");
+const app = require("./app.js");
 
 //* Load env
 dotenv.config();
@@ -8,24 +43,21 @@ dotenv.config();
 //* Database connection
 (async () => {
   try {
-    // const conn = await mongoose.connect(process.env.MONGO_URI,{
-    //   authSource:'admin',
-    //   useNewUrlParser:true,
-    //   useUnifiedTopology:true
-    // });
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    //?error catch
-    console.log(err);
+    console.log("MongoDB Connection Error:", err);
     process.exit(1);
   }
 })();
 
-// const port = +process.env.PORT || 3000;
-const port = 4000;
-
+const port = process.env.PORT || 3000;
 const productionMode = process.env.NODE_ENV === "production";
+
 app.listen(port, () => {
   console.log(`Server running in ${productionMode ? "production" : "development"} mode on port ${port}`);
 });
+
+
+
+
